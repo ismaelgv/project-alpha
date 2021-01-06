1c54e19ffe893a120ef915bf728400529089fdcd
Ismael González <ismgonval@gmail.com>
2021-01-06 21:21:30 +0100

    Remove unneeded test in CRUD

--- services/crud/src/test/java/igv/projects/alphacrud/AlphaBackendApplicationTests.java
+++ services/crud/src/test/java/igv/projects/alphacrud/AlphaBackendApplicationTests.java
@@ -1,13 +1,8 @@
 package igv.projects.alphacrud;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AlphaBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
