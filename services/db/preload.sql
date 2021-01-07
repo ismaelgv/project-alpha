a5be68786f4bd03fae947f4288253bcbfc7672ae
Ismael González <ismgonval@gmail.com>
2021-01-08 00:42:28 +0100

    Add questions and answers to preload

---
+++ services/db/preload.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS question (
+    id BIGSERIAL PRIMARY KEY,
+    mood INTEGER,
+    question VARCHAR(255)
+);
+INSERT INTO question (question)
+VALUES
+    ('Will I be the next president of the world?'),
+    ('Will I be the next Steve Jobs?'),
+    ('Will I be rich and famous?'),
+    ('Will I revolutionize the world?'),
+    ('Will future generations remember me as the new Mesiah?');
+
+
+CREATE TABLE IF NOT EXISTS answer (
+    id BIGSERIAL PRIMARY KEY,
+    mood INTEGER,
+    answer VARCHAR(255)
+);
+INSERT INTO answer (answer)
+VALUES
+    ('You are great and you will achieve anything you want in this life.'),
+    ('You are going to be even better than that.'),
+    ('The whole industry envy you.'),
+    ('Today is the next day in the road to your success.'),
+    ('You are awesome.'),
+    ('Can I send you my CV?');
